5e9e04032e017255789a982c307b09996b143b1e
Chimbal <chimbal.chimbal@yandex.ru>
2026-03-26 20:52:25 +0300

    Update Main_GUI_MID_Results.gd

--- Src/Main_GUI_MID_Results.gd
+++ Src/Main_GUI_MID_Results.gd
@@ -3,12 +3,13 @@ extends Control
 const btn_select_list = "/root/Main/GUI_MID/Results/Select_List"
 const btn_select_list_scene = "/root/Main/GUI_MID/Results/Select_List_Scene"
 
-var file_with_results = File.new()
-var file_with_results_code = null
-var file_with_results_path = null
+var FileWithResults = File.new()
+var FileWithResultsCode = null
+var FileWithResultsPath = null
+var ResultsT = []
 
 const result_files = {
-"Simple360pComplex":"/DB/Results-Simple-360p-Complex.txt",
+"Simple360pComplex":"/DB/Results-Simple-360p-Complexe.txt",
 "Simple720pComplex":"/DB/Results-Simple-720p-Complex.txt",
 "Shader360pComplex":"/DB/Results-Shader-360p-Complex.txt",
 "Shader720pComplex":"/DB/Results-Shader-720p-Complex.txt",
@@ -26,24 +27,21 @@ const result_files = {
 "Shader720pWINE":"/DB/Results-Shader-720p-WINE.txt",
 }
 
-var ResultsT = []
-
 func _ready():
 	select_list_fill()
 	select_list_scene_fill()
-	list_fill()
+	list_fill("Simple360pComplex")
 
-func list_fill():
-	var db_simple_640 = str(get_node("/root/Main").main_execute_path+result_files["Simple360pComplex"])
-	file_with_results_path = db_simple_640
-	if file_with_results.open(file_with_results_path, File.READ) == OK:
-		while not file_with_results.eof_reached():
-			ResultsT.append(file_with_results.get_line())
-		file_with_results.close()
-	
-	for item in ResultsT:
-		get_node("ItemList").add_item(" "+str(item))
-	
+func list_fill(name):
+	var FileWithResultsPath = str(get_node("/root/Main").main_execute_path+result_files[name])
+	if FileWithResults.open(FileWithResultsPath, File.READ) == OK:
+		while not FileWithResults.eof_reached():
+			ResultsT.append(FileWithResults.get_line())
+		FileWithResults.close()
+		for item in ResultsT:
+			get_node("ItemList").add_item(" "+str(item))
+	else:
+		get_node("ItemList").add_item("Error opening file: "+str(FileWithResultsPath))
 
 func select_list_fill():
 	get_node(btn_select_list).add_item("Complex - 640x360", 0)
