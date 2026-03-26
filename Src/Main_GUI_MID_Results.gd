c82c66c08ced71ed30593f564803cb38f06c218f
Chimbal <chimbal.chimbal@yandex.ru>
2026-03-26 20:45:42 +0300

    db clean

--- Src/Main_GUI_MID_Results.gd
+++ Src/Main_GUI_MID_Results.gd
@@ -8,10 +8,22 @@ var file_with_results_code = null
 var file_with_results_path = null
 
 const result_files = {
-"Simple360p":"/DB/Results-Simple-360p.txt",
-"Simple720p":"/DB/Results-Simple-720p.txt",
-"Shader360p":"/DB/Results-Shader-360p.txt",
-"Shader720p":"/DB/Results-Shader-720p.txt",
+"Simple360pComplex":"/DB/Results-Simple-360p-Complex.txt",
+"Simple720pComplex":"/DB/Results-Simple-720p-Complex.txt",
+"Shader360pComplex":"/DB/Results-Shader-360p-Complex.txt",
+"Shader720pComplex":"/DB/Results-Shader-720p-Complex.txt",
+"Simple360pWindows":"/DB/Results-Simple-360p-Windows.txt",
+"Simple720pWindows":"/DB/Results-Simple-720p-Windows.txt",
+"Shader360pWindows":"/DB/Results-Shader-360p-Windows.txt",
+"Shader720pWindows":"/DB/Results-Shader-720p-Windows.txt",
+"Simple360pLinux":"/DB/Results-Simple-360p-Linux.txt",
+"Simple720pLinux":"/DB/Results-Simple-720p-Linux.txt",
+"Shader360pLinux":"/DB/Results-Shader-360p-Linux.txt",
+"Shader720pLinux":"/DB/Results-Shader-720p-Linux.txt",
+"Simple360pWINE":"/DB/Results-Simple-360p-WINE.txt",
+"Simple720pWINE":"/DB/Results-Simple-720p-WINE.txt",
+"Shader360pWINE":"/DB/Results-Shader-360p-WINE.txt",
+"Shader720pWINE":"/DB/Results-Shader-720p-WINE.txt",
 }
 
 var ResultsT = []
@@ -22,7 +34,7 @@ func _ready():
 	list_fill()
 
 func list_fill():
-	var db_simple_640 = str(get_node("/root/Main").main_execute_path+result_files["Simple360p"])
+	var db_simple_640 = str(get_node("/root/Main").main_execute_path+result_files["Simple360pComplex"])
 	file_with_results_path = db_simple_640
 	if file_with_results.open(file_with_results_path, File.READ) == OK:
 		while not file_with_results.eof_reached():
