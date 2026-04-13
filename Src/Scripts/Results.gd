8f6a79bf1769317272024009cd7dc41b5666db97
Chimbal <chimbal.chimbal@yandex.ru>
2026-04-13 13:31:12 +0300

    pre release

--- Src/Scripts/Results.gd
+++ Src/Scripts/Results.gd
@@ -2,8 +2,8 @@ extends Control
 
 const btn_select_list = "/root/Main/GUI_MID/Results/Select_List"
 const btn_select_list_scene = "/root/Main/GUI_MID/Results/Select_List_Scene"
-var selected_scene = "Simple"
-var selected_platform = "Complex"
+var selected_scene = "PreHeat"
+var selected_platform = "Windows"
 
 var FileWithResults = File.new()
 var FileWithResultsCode = null
@@ -25,7 +25,7 @@ const result_files = {
 func _ready():
 	select_list_fill()
 	select_list_scene_fill()
-	list_fill("Simple", "Complex")
+	list_fill("PreHeat", "Windows")
 	set_process_input(true)
 
 func list_fill(test, platform):
@@ -42,23 +42,24 @@ func list_fill(test, platform):
 		get_node("ItemList").add_item("Error opening file: "+str(FileWithResultsPath))
 
 func select_list_fill():
-	get_node(btn_select_list).add_item("Complex", 0)
-	get_node(btn_select_list).add_item("Windows", 1)
-	get_node(btn_select_list).add_item("Linux", 2)
+	get_node(btn_select_list).add_item("Windows", 0)
+	get_node(btn_select_list).add_item("Linux", 1)
+	get_node(btn_select_list).add_item("Complex", 2)
 
 func select_list_scene_fill():
-	get_node(btn_select_list_scene).add_item("Simple Test", 0)
-	get_node(btn_select_list_scene).add_item("Shader Test", 1)
-	get_node(btn_select_list_scene).add_item("PreHeat Test", 2)
+	get_node(btn_select_list_scene).add_item("PreHeat Test", 0)
+	get_node(btn_select_list_scene).add_item("Simple Test", 1)
+	get_node(btn_select_list_scene).add_item("Shader Test", 2)
+	
 
 func _on_Select_List_Scene_item_selected( ID ):
-	if ID == 0: selected_scene = "Simple"
-	if ID == 1: selected_scene = "Shader"
-	if ID == 2: selected_scene = "PreHeat"
+	if ID == 0: selected_scene = "PreHeat"
+	if ID == 1: selected_scene = "Simple"
+	if ID == 2: selected_scene = "Shader"
 	list_fill(selected_scene, selected_platform)
 
 func _on_Select_List_item_selected( ID ):
-	if ID == 0: selected_platform = "Complex"
-	elif ID == 1: selected_platform = "Windows"
-	elif ID == 2: selected_platform = "Linux"
+	if ID == 0: selected_platform = "Windows"
+	elif ID == 1: selected_platform = "Linux"
+	elif ID == 2: selected_platform = "Complex"
 	list_fill(selected_scene, selected_platform)
